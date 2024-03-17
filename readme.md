ece94b7e6b7ae3ed36e03ffc8aa2919af4aa7a0d
hcncc <sbombastic@gmail.com>
2024-03-17 10:52:39 +0100

    feat: refactoring

---
+++ readme.md
@@ -0,0 +1,3 @@
+# The first API using Golang
+
+* By.: António Januário João (Wander Jack)
--- router/router.go
+++ router/router.go
@@ -3,12 +3,17 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
+
+	printMeFunc()
+
 	router := gin.Default()
 	router.GET("/test", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"hello": "world",
 		})
 	})
+
+	// router.GET("/me", )
 
 	router.Run(":8089") //listening port 8089
 }
