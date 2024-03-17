d9d5525f9f936e76d103217d2aef0aed70a1210f
hcncc <sbombastic@gmail.com>
2024-03-17 14:06:53 +0100

    feat: Database, modularization add

--- router/routes.go
+++ router/routes.go
@@ -2,14 +2,14 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/hcncc/gopportunities/handler"
+	handler "github.com/hcncc/gopportunities/handler/Opening"
 )
 
 func initializerRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1/")
 	{
 		// Show one opening
-		v1.GET("/opening/id", handler.Opening.Show.ShowOpeningHandler)
+		v1.GET("/opening/id", handler.ShowOpeningHandler)
 
 		// Store opening
 		v1.POST("/opening", handler.StoreOpeningHandler)
@@ -17,8 +17,10 @@ func initializerRoutes(router *gin.Engine) {
 		//Update opening
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 
+		// Delete opening
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
 
+		// FindMany opening
 		v1.GET("/openings", handler.IndexOpeningHandler)
 	}
 }
